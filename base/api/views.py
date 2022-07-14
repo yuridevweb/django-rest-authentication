9dc9f8f5b0d80ee933cc1503c65ce117a60679c0
yuridevweb <yuridevweb@gmail.com>
2022-07-14 11:34:47 +0100

    adding notes endpoints

--- base/api/views.py
+++ base/api/views.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .serializers import NoteSerializer
+from base.models import Note
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -30,3 +34,12 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
